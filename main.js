6b49a09330d9bc8d6db5be37f6d1bc4680966525
Bruno Inec <brunoinec@gmail.com>
2018-09-18 18:39:02 +0200

    Actually working alarm

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+var confirm_text = "yes i am";
+var prompt_text = `Are you woken up yet?
+(enter "${confirm_text}" to confirm you are)`;
+
 function removeAlarm(alarm) {
     return function() {
         alarm.parentElement.removeChild(alarm);
@@ -8,6 +12,7 @@ function addAlarm() {
     var alarm_list = document.querySelector('#alarm-list');
     var alarm_template = document.querySelector('#alarm');
     var alarm = document.importNode(alarm_template.content, true);
+    var timeout;
 
     // Insert new alarm and get a reference to it instead of just a clone
     // of the template
@@ -38,7 +43,12 @@ function addAlarm() {
 function updateClock() {
     var clock = document.getElementsByTagName('time')[0];
     var now = new Date();
-    var time = `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
+
+    var hours = String(now.getHours()).padStart(2, 0);
+    var minutes = String(now.getMinutes()).padStart(2, 0);
+    var seconds = String(now.getSeconds()).padStart(2, 0);
+
+    var time = `${hours}:${minutes}:${seconds}`;
     clock.innerHTML = time;
 }
 
@@ -49,10 +59,32 @@ function changeColorCheckedBox() {
     }
 }
 
+function checkAlarms() {
+    var alarm_list = document.querySelector('#alarm-list');
+    for(let alarm of alarm_list.children) {
+        var checkbox = alarm.querySelector('input[type=checkbox]');
+        if (checkbox.checked) {
+            let current_time = new Date();
+            let target_time = alarm.querySelector('input[type=time]').valueAsDate;
+            if (target_time.getUTCHours() === current_time.getHours() &&
+                target_time.getUTCMinutes() === current_time.getMinutes() &&
+                current_time.getSeconds() === 0) {
+                let music = new Audio('music/' + alarm.querySelector('select').value + '.mp3');
+                music.play();
+                
+                while (prompt(prompt_text) !== confirm_text) {
+                    continue;
+                }
+                music.pause();
+            }
+        }
+    }
+}
+
 window.addEventListener('load', function () {
     var add_button = document.querySelector('#add-alarm');
 
     add_button.addEventListener('click', addAlarm);
     setInterval(updateClock, 1000);
+    setInterval(checkAlarms, 1000);
 });
-    
